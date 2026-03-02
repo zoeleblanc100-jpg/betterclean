443c5b1a5fb08d7e97684b68fc1940d7e2135ab4
Happy Cat Toys <dev@happycattoys.ca>
2026-03-02 17:05:39 -0500

    Restore second image to original SHOP1.webp as requested by user

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ export default function LandingPage() {
             </div>
             <div className="relative w-full h-auto">
               <Image 
-                src="/image_amelioree.png" 
+                src="/SHOP1.webp" 
                 alt="BetterClean in Action"
                 width={600}
                 height={400}
